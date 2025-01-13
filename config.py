caa237eb15a0e3e968b0c9c8b75b68ac4b52af94
DominikKaiser74 <95078348+DominikKaiser74@users.noreply.github.com>
2025-01-13 13:46:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 db_host = "10.100.160.169"
 db_port = 3306
-db_user = "bitnami"
-db_password = "bitnami"
+db_user = "user"
+db_password = "password"
 db_database = "apothekendienst"
 Karfreitag = False
 CSV_URL = 'https://www.wien.gv.at/gogv/l9ogdfeiertagskalender'
